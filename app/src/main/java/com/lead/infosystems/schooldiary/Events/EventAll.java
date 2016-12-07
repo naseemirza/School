b9dd7b9a64a6556cd0dde63c652d89f2bca53d1b
Jyotichoudhary <jyotichdhr89@gmail.com>
2016-12-07 11:42:44 +0530

    jyoti

--- app/src/main/java/com/lead/infosystems/schooldiary/Events/EventAll.java
+++ app/src/main/java/com/lead/infosystems/schooldiary/Events/EventAll.java
@@ -1,109 +1,158 @@
 package com.lead.infosystems.schooldiary.Events;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+import com.lead.infosystems.schooldiary.Data.UserDataSP;
 import com.lead.infosystems.schooldiary.R;
+import com.lead.infosystems.schooldiary.ServerConnection.Utils;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link EventAll.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link EventAll#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 public class EventAll extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
-
+    UserDataSP userDataSP;
+    CompactCalendarView calendarView;
+    ListView listview;
+    private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMM - yyyy", Locale.getDefault());
     public EventAll() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EventAll.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EventAll newInstance(String param1, String param2) {
-        EventAll fragment = new EventAll();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_all, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_event_all, container, false);
+        userDataSP=new UserDataSP(getActivity());
+
+        calendarView = (CompactCalendarView)rootView.findViewById(R.id.compactcalendar_view);
+        getActivity().setTitle(dateFormatForMonth.format(calendarView.getFirstDayOfCurrentMonth()));
+
+        getEventData();
+        return rootView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    public void getEventData()
+    {
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        StringRequest request = new StringRequest(Request.Method.POST, Utils.ATTENDANCE_FETCH, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+
+                try {
+                    getJsonData(response);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> params = new HashMap<>();
+
+                params.put("number_user",userDataSP.getUserData(UserDataSP.NUMBER_USER));
+
+                return params;
+            }
+        };
+        RetryPolicy retryPolicy = new DefaultRetryPolicy(2000,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        request.setRetryPolicy(retryPolicy);
+        requestQueue.add(request);
+
+
+    }
+
+    private void getJsonData(String re) throws JSONException {
+        JSONArray json = new JSONArray(re);
+
+
+        for (int i = 0; i <= json.length() - 1; i++) {
+            JSONObject jsonobj = json.getJSONObject(i);
+           // attendance.add(new AttendanceData(jsonobj.getString("year"), jsonobj.getString("day"), jsonobj.getString("month"), jsonobj.getString("attendance")));
+
         }
+        getEventsDetail();
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    private void getEventsDetail()
+    {
+        Event e;
+//        for(int i=0; i<attendance.size(); i++) {
+//            AttendanceData allAttendance = attendance.get(i);
+//            String year = allAttendance.getYear();
+//            String day= allAttendance.getDay();
+//            String month =  allAttendance.getMonth();
+//            if(allAttendance.getAttendance().contains("A")) {
+//                e = new Event(Color.RED, Utils.getTimeMiliSec(year + "-" + month + "-" + day + " " + 10 + ":" + 20 + ":" + 12), allAttendance.getAttendance());
+//                calendarView.addEvent(e);
+//            }
+//            else if(allAttendance.getAttendance().contains("L"))
+//            {
+//                e = new Event(Color.YELLOW, Utils.getTimeMiliSec(year + "-" + month + "-" + day + " " + 10 + ":" + 20 + ":" + 12), allAttendance.getAttendance());
+//                calendarView.addEvent(e);
+//            }
+//            else
+//            {
+//                e = new Event(Color.GREEN, Utils.getTimeMiliSec(year + "-" + month + "-" + day + " " + 10 + ":" + 20 + ":" + 12), allAttendance.getAttendance());
+//                calendarView.addEvent(e);
+//            }
+//        }
+        calendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                //List<Event> events = calendarView.getEvents(dateClicked);
+                Toast.makeText(getActivity(), "There is no data", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                getActivity().setTitle(dateFormatForMonth.format(firstDayOfNewMonth.getTime())+"");
+            }
+        });
+
+
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+
+
+
+
+
+
+
 }
